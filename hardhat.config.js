4df3766f1e2c8850654f333508230bc63ecd633b
Ravindra7330 <ravindarravi87@gmail.com>
2025-03-07 13:49:03 +0530

    deployed to sepolia testnet

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
-
+require("dotenv").config();
+const { INFURA_API_KEY, PRIVATE_KEY } = process.env;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    hardhat:{},
+    sepolia: {
+      url: `${INFURA_API_KEY}` ,
+      accounts: [PRIVATE_KEY]
+    },
+  },
 };
